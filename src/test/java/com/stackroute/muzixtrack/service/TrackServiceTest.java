8ac9720f06cbb52ef8fb75ff9f9361c9afa0dd13
keerthika9 <52036271+keerthika9@users.noreply.github.com>
2019-08-12 18:10:43 +0530

    Update TrackServiceTest.java

--- src/test/java/com/stackroute/muzixtrack/service/TrackServiceTest.java
+++ src/test/java/com/stackroute/muzixtrack/service/TrackServiceTest.java
@@ -1,127 +1,92 @@
-package com.stackroute.muzixtrack.service;
+package com.stackroute.muzixtrack.repository;
+
 import com.stackroute.muzixtrack.domain.Track;
-import com.stackroute.muzixtrack.exceptions.TrackAlreadyExistsException;
-import com.stackroute.muzixtrack.exceptions.TrackNotFoundException;
-import com.stackroute.muzixtrack.repository.TrackRepository;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.internal.matchers.Null;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.client.ExpectedCount.times;
 
-public class TrackServiceTest {
- private Track track;
-  //Create a mock for UserRepository
-  @Mock
-  TrackRepository trackRepository;
-  //Inject the mocks as dependencies into UserServiceImpl
-  @InjectMocks
-  TrackServiceImpl trackService;
-  List<Track> list = null;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+//When a class is annotated with @RunWith or extends a class annotated with @RunWith, JUnit will invoke the class it references to runthe tests in that class instead of the runner built into JUnit
+@RunWith(SpringRunner.class)//The SpringRunner provides support for loading a Spring ApplicationContext and having beans
+//@DataJpaTest
+@SpringBootTest
+public class TrackRepositoryTest {
+  @Autowired//@Autowired annotation is used for automatic dependency injection
+   private TrackRepository trackRepository;
+  private Track track;
+
   @Before
-  public void setUp() throws Exception {
-//Initialising the mock object
-    MockitoAnnotations.initMocks(this);
-    track=new Track();
-    track.setId(9);
-    track.setName("ekbaar");
-    track.setComments("nice");
-    list = new ArrayList<>();
-    list.add(track);
+  public void setup() {
+    track = new Track();
+    track.setId(1);
+    track.setName("keer");
+    track.setComments("super");
   }
+
   @After
-  public void tearDown() throws Exception {
-   track=null;
+  public void teardown() {
+    track=null;
+  }
+
+@Test
+  public void givenDetailsAsInputShouldReturnCorrectId() {
+    trackRepository.save(track);
+    Track trackDetails = trackRepository.findById(track.getId());
+    assertEquals(1, trackDetails.getId());
   }
   @Test
-  public void saveTrackSuccess() throws TrackAlreadyExistsException {
-    when(trackRepository.existsById(track.getId())).thenReturn(false);
-    when(trackRepository.save((Track)any())).thenReturn(track);
-    Track saveTrack = trackService.save(track);
-    Assert.assertEquals(track, saveTrack);
-    //verify here verifies that trackRepository save method is only called ones
-    verify(trackRepository, Mockito.times(1)).save(track);
+  public void givenNameAsInputShouldReturnTrackDetails() {
+    Track track = new Track(2, "akhi", "great");
+    trackRepository.save(track);
+    List<Track> list = trackRepository.findAll();
+    Assert.assertEquals("akhi", list.get(0).getName());
   }
- @Test(expected = TrackAlreadyExistsException.class)
-  public void givenTrackAsInputShouldReturnSaveTrackTestFailure() throws TrackAlreadyExistsException {
-    when(trackRepository.save(any())).thenReturn(null);
-    Track savedTrack = trackService.save(track);
-    System.out.println("savedTrack" + savedTrack);
-   verify(trackRepository, Mockito.times(0)).save(track);
+@Test
+  public void givenIdAsInputShouldReturnTrackFailure(){
+  trackRepository.save(track);
+  Track trackDetails = trackRepository.findById(track.getId());
+  Assert.assertNotSame(3,trackDetails.getId());
+  }
+  @Test
+  public void givenDetailsAsInputShouldReturnSavedTrack() {
+    Track trackInput = new Track(3, "lakalaka", "horror");
+    trackRepository.save(trackInput);
+    List<Track> trackDetails = trackRepository.findAll();
+    List<Track> expectedDtails = new ArrayList<>();
+    expectedDtails.add(trackInput);
+    assertEquals(expectedDtails, trackDetails);
+  }
+
+  @Test
+  public void givenTrackShouldDeleteTrackById(){
+    trackRepository.save(track);
+    trackRepository.deleteById(track.getId());
+    assertEquals(true, trackRepository.existsById(track.getId()));
 
   }
   @Test
-  public void givenDataAsInputShouldReturnAllTracks() throws Exception {
-    trackRepository.save(track);
-    //stubbing the mock to return specific data
-    when(trackRepository.findAll()).thenReturn(list);
-    List<Track> trackList = trackService.getAllTracks();
-    Assert.assertEquals(list, trackList);
-   verify(trackRepository, Mockito.times(1)).getAllTracks(track);
-  }
-  @Test
-  public void givenIdAsInputShouldReturnTrack() throws TrackNotFoundException {
-    trackRepository.save(track);
-    when(trackRepository.existsById(9)).thenReturn(true);
-    when(trackRepository.findById(9)).thenReturn(track);
-    Track getTrack=trackService.getById(9);
-    Assert.assertEquals(track,getTrack);
-  }
-  @Test
-  public void givenNameAsInputShouldReturnTrack()throws Exception{
-    trackRepository.save(track);
-    when(trackRepository.findByName(any())).thenReturn(list);
-    List<Track> getTrack=trackService.findByName("ekbaar");
-    Assert.assertEquals(list,getTrack);
-   verify(trackRepository, Mockito.times(1)).findByName(track);
-  }
-  @Test
-  public void givenIdAsInputShouldDeleteTrack()throws TrackNotFoundException{
-    trackRepository.save(track);
-    when(trackRepository.existsById(9)).thenReturn(true);
-    when(trackRepository.findById(9)).thenReturn(track);
-    Track deleteTrack=trackService.deleteById(9);
-    Assert.assertEquals(track,deleteTrack);
-   verify(trackRepository, Mockito.times(1)).deleteById(track);
-  }
-  @Test
-  public void givenIdAsInputShouldUpdateTrack()throws TrackNotFoundException{
-    trackRepository.save(track);
-    when(trackRepository.existsById(9)).thenReturn(true);
-    when(trackRepository.findById(9)).thenReturn(track);
-    Track updateTrack=trackService.updateTrackById(9,track);
-    Assert.assertEquals(track,updateTrack);
-   verify(trackRepository, Mockito.times(1)).updateTrackById(track);
-  }
+  public void gievnTrackShouldUpdateTrackById() {
+    Track track1 = new Track(1, "SAHO", "title track");
+    Track track2 = new Track(2, "LAKA", "intro song");
+    trackRepository.save(track1);
+    trackRepository.save(track2);
 
-  @Test(expected = TrackAlreadyExistsException.class)
-  public void saveUserTestFailure() throws TrackAlreadyExistsException {
-    when(trackRepository.save(any())).thenReturn(null);
-    Track savedTrack = trackService.save(track);
-    System.out.println("savedTrack" + savedTrack);
-    //Assert.assertEquals(user,savedUser);
-      /*doThrow(new UserAlreadyExistException()).when(userRepository).findById(eq(101));
-      userService.saveUser(user);*/
-  }
-  @Test(expected = TrackNotFoundException.class)
-    public void givenNegativeIdAsInputShouldReturnTrackNotFound() throws TrackNotFoundException {
-    trackRepository.save(track);
-    //stubbing the mock to return specific data
-    when(trackRepository.findById(9)).thenReturn(track);
-    Track gettrack = trackService.getById(9);
-    Assert.assertEquals(track, gettrack);
-   verify(trackRepository, Mockito.times(1)).findById(track);
+
+    Track trackList = trackRepository.findById(track1.getId());
+    trackList.setName(track2.getName());
+
+    assertEquals(trackList.getName(), track2.getName());
+
   }
 }
-
-
